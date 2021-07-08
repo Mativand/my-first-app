7fd371d962744d7f056b6477284db9ba7b28bcef
Your Name <ivandaev9@gmail.com>
2021-07-08 23:00:29 +0700

    add new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='http://demo.foxthemes.net/socialitev2.1/assets/images/logo.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div className="img-background">
+          <img src='https://www.rentacarefex.com/assets/img/blog/rent-a-car-beograd-efex.jpg' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
