5be9068ac3b983d827327cca75556e0855d9fa2e
Your Name <ivandaev9@gmail.com>
2021-07-15 23:00:07 +0700

    added component DialogItem and Message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,54 +2,43 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    return <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+}
+
+const Message = (props) => {
+    return <div>{props.message}</div>
+}
+
 const Dialogs = (props) => {
     return (
-      <div className={s.dialogs}>
-          <div className={s.dialogsItems}>
-              <div className={s.dialog + ' ' + s.active}>
-                 <NavLink to="/dialogs/1">Ilya</NavLink>
-              </div>
-              <div className={s.dialog+ '' +s.active}>
-                  <NavLink to="/dialogs/2">Sasha</NavLink>
-              </div>
-              <div className={s.dialog+ '' +s.active}>
-                  <NavLink to="/dialogs/3">Yana</NavLink>
-              </div>
-              <div className={s.dialog+ '' +s.active}>
-                  <NavLink to="/dialogs/4">Andrey</NavLink>
-              </div>
-              <div className={s.dialog+ '' +s.active}>
-                  <NavLink to="/dialogs/5">Aleksey</NavLink>
-              </div>
-          </div>
-          <div className={s.messages}>
-                <div className={s.message}>
-                    <div>Hi!</div>
-                    <div>How work?</div>
-                    <div>Nice!</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <DialogItem name="Ilya" id="1"/>
                 </div>
-              <div className={s.message}>
-                  <div>Hi!</div>
-                  <div>Do you have food?</div>
-                  <div>Nice!</div>
-              </div>
-              <div className={s.message}>
-                  <div>Hi!</div>
-                  <div>Please help me</div>
-                  <div>Thanks!</div>
-              </div>
-              <div className={s.message}>
-                  <div>...!</div>
-                  <div>Yes</div>
-                  <div>Ok!</div>
-              </div>
-              <div className={s.message}>
-                  <div>Hi!</div>
-                  <div>bla bla?</div>
-                  <div>Nice!</div>
-              </div>
-          </div>
-      </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <DialogItem name="Sasha" id="2"/>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <DialogItem name="Yana" id="3"/>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <DialogItem name="Andrey" id="4"/>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <DialogItem name="Aleksey" id="5"/>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    <Message message="Hi"/>
+                    <Message message="How work?"/>
+                    <Message message="Nice!"/>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
